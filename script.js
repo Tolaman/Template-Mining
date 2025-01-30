cdaaf5dc9e41209641ab995f328aae1fb669b80b
Michee Luzayisu <michemakaya11@gmail.com>
2025-01-30 23:07:42 +0200

    Ajusting the video and the list of information on the main page

--- script.js
+++ script.js
@@ -12,3 +12,16 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElement = document.querySelectorAll('.hidden-element');
 hiddenElement.forEach((el) => observer.observe(el));
+
+///////////////////////////////////////////////////////////////////////////
+const mutedButton = document.getElementById("muted-unmute-button");
+const video = document.getElementById("myVideo");
+mutedButton.addEventListener("click", () => {
+    if (video.muted) {
+        video.muted = false; // Unmutes the video
+        mutedButton.innerHTML = '<i class="fa-solid fa-volume-high"></i>'; // Change the button icon
+    } else {
+        video.muted = true; // Mutes the video
+        mutedButton.innerHTML = '<i class="fa-solid fa-volume-xmark"></i>'; // Change the button icon
+    }
+});
