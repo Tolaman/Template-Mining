632a74d0b66718573f934eb0a682a9803a028ffb
Michee Luzayisu <michemakaya11@gmail.com>
2025-03-01 23:24:13 +0200

    inserted bootstrap slider into the main page

--- script.js
+++ script.js
@@ -14,40 +14,6 @@ const hiddenElement = document.querySelectorAll('.hidden-element');
 hiddenElement.forEach((el) => observer.observe(el));
 
 ///////////////////////////////////////////////////////////////////////////
-
-let items = document.querySelectorAll('.main-slider .list .item');
-let next = document.getElementById('next');
-let prev = document.getElementById('prev');
-let thumnail = document.querySelectorAll('.thumnail .item');
-
-let countItem = items.length;
-let itemActive = 0;
-
-next.onclick = function () {
-    itemActive++;
-    if(itemActive >= countItem + 1) {
-        itemActive = 0;
-    }
-    showSlider();
-}
-
-prev.onclick = function () {
-    itemActive--;
-    if(itemActive < 0) {
-        itemActive = countItem - 1;
-    }
-    showSlider();
-}
-function showSlider() {
-    let itemActiveold = document.querySelector('.main-slider .list .item');
-    let thumnailActiveOld = document.querySelector('.thumnail .item.active');
-    itemActiveold.classList.remove('active');
-    thumnailActiveOld.classList.remove('active');
-
-    items[itemActive].classList.add('active');
-    thumnail[itemActive].classLoist.add('active');
-}
-
 ///////////////////////////////////////////////////////////////////////////
 const mutedButton = document.getElementById("muted-unmute-button");
 const video = document.getElementById("myVideo");
